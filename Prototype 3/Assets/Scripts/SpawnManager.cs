c8e6dde0b05ff4e0123dec226d8228b246334d85
Kamidreams <105478024+Kamidreams@users.noreply.github.com>
2022-10-11 11:03:52 -0700

    Obsticle

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsticlePrefab;
+
+    private Vector3 _spawnPos = new Vector3(15, -3, 0);
+
+    private float _startDelay = 2;
+
+    private float _repeatRate = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      InvokeRepeating("SpawnObsticle", _startDelay, _repeatRate);  
+    }
+
+    void SpawnObsticle()
+    {
+        Instantiate(obsticlePrefab, _spawnPos, obsticlePrefab.transform.rotation);
+    }
+}
